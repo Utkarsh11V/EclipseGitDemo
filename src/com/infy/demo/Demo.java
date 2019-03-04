383edbdaa4c267956f112c4ce03a5d8390a1752a
utkarsh <utkarsh.v1101@gmail.com>
2019-03-04 18:47:28 +0530

    second local change

--- src/com/infy/demo/Demo.java
+++ src/com/infy/demo/Demo.java
@@ -3,5 +3,6 @@ package com.infy.demo;
 public class Demo {
 		public static void main(String[] args) {
 			System.out.println("First line for git demo...");
+			System.out.println("second line...");
 		}
 }
