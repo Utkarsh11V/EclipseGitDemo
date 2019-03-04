fd3446f0d64123dd9077841389b9d021eb80099c
Utkarsh11V <36992861+Utkarsh11V@users.noreply.github.com>
2019-03-04 19:00:27 +0530

    Upate Demo on remote

--- src/com/infy/demo/Demo.java
+++ src/com/infy/demo/Demo.java
@@ -3,6 +3,7 @@ package com.infy.demo;
 public class Demo {
 		public static void main(String[] args) {
 			System.out.println("First line for git demo...");
-			System.out.println("second line...");
+			System.out.println("second line on local chnage...");
+			System.out.println("third line on remote chnage..");
 		}
 }
